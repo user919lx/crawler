2bccd96c31416a4d4168cb7e12159197d0b121d4
xinlan <xinlan@tencent.com>
2022-03-25 17:42:52 +0800

    feat:修改数据库配置 --other

--- switch/switch/settings.py
+++ switch/switch/settings.py
@@ -73,7 +73,7 @@ MYSQL_CONFIG = {
     "user": os.environ.get("MYSQL_USER", "root"),
     "password": os.environ.get("MYSQL_PWD", "root_raw_password"),
     "database": os.environ.get("MYSQL_DB", "switch"),
-    "port": 3308,
+    "port": 3306,
     "charset": "utf8mb4",
 }
 
